97924bad1342c9013f0ad1eebc021b34483d4219
cherrytrees-kpu <54606285+cherrytrees-kpu@users.noreply.github.com>
2020-10-21 14:01:32 -0700

    addTranscripts.py
    Enables importing of all of the transcripts into the database.

---
+++ addtranscripts.py
@@ -0,0 +1,63 @@
+import sqlite3
+import anno
+
+def getTranscriptInfo (transcript, hgvs):
+
+    data = [
+        hgvs,
+        transcript['gene_id'], 
+        transcript['gene_symbol'],
+        transcript['transcript_id'],
+        transcript['consequence_terms'][0],
+        transcript['strand'],
+    ]
+    if 'canonical' in transcript: 
+        data.append(transcript['canonical'])
+    else: 
+        data.append(0)
+
+    if 'cdna_start' in transcript:
+        data.append(transcript['cdna_start'])
+        data.append(transcript['cdna_end'])
+    else:
+        data.append(None)
+        data.append(None)
+
+    if 'cds_start' in transcript:
+        data.append(transcript['cds_start'])
+        data.append(transcript['cds_end'])
+    else:
+        data.append(None)
+        data.append(None)
+
+    if 'protein_start' in transcript:
+        data.append(transcript['protein_start'])
+        data.append(transcript['protein_end'])
+    else:
+        data.append(None)
+        data.append(None)
+
+    if 'amino_acids' in transcript:
+        data.append(transcript['amino_acids'])
+    else: 
+        data.append(None)
+
+    return tuple(data)
+
+#Database access
+connection = sqlite3.connect('family_M.db')
+cursor = connection.cursor()
+
+#Import annotations
+annoList = anno.get("")
+
+for anno in annoList: 
+    if 'transcript_consequences' in anno['vep']:
+        #Get transcript info
+        for transcript in anno['vep']['transcript_consequences']:
+            transcriptInfo = getTranscriptInfo(transcript, anno['vep']['id'])
+            #Add to table
+            cursor.execute('INSERT INTO Transcripts(HGVS, Gene, EnsemblGene, EnsemblTranscript, Consequence, Canonical, Strand, cdna_start, cdna_end, cds_start, cds_end, protein_start, protein_end, amino_acids) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', transcriptInfo)
+            connection.commit()
+
+connection.close()
--- createDatabase.py
+++ createDatabase.py
@@ -8,6 +8,7 @@ cursor = connection.cursor()
 cursor.execute('DROP TABLE IF EXISTS GeneralInformation')
 cursor.execute('CREATE TABLE GeneralInformation (HGVS TEXT, RSID TEXT, VarType TEXT, MostSevereConsequence TEXT, gnomADE FLOAT, gnomADG FLOAT, Gene TEXT, EnsemblGene TEXT, CADDRaw FLOAT, CADDPhred FLOAT)')
 #cursor.execute('CREATE TABLE InSilicoModel (HGVS TEXT, Gene TEXT, UniprotACC TEXT, UniprotEntry TEXT, ProveanPred TEXT, ProveanScore FLOAT, ProveanRank FLOAT, Polyphen2Pred TEXT, Polyphen2Score FLOAT, Polyphen2Rank FLOAT, SIFTPred TEXT, SiftScore FLOAT, SiftRank FLOAT, CADDRaw FLOAT, CADDPhred FLOAT)')
+cursor.execute('CREATE TABLE Transcripts (HGVS TEXT, Gene TEXT, EnsemblGene TEXT, EnsemblTranscript TEXT, Consequence TEXT, Strand INTEGER, Canonical INTEGER, cdna_start INTEGER, cdna_end INTEGER, cds_start INTEGER, cds_end INTEGER, protein_start INTEGER, protein_end INTEGER, amino_acids TEXT)')
 
 #Write to table
 #cursor.execute('INSERT INTO GeneralInformation(HGVS, RSID, VarType, gnomADE, gnomADG, Gene, EnsemblGene) VALUES (?, ?, ?, ?, ?, ?, ?)', ('Bob', 'is', 'your', 0.00, 0.00, 'Uncle', 'Boy'))
