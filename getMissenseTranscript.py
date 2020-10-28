a4eebc468cdd7bdfa7b39b97f10581c7010374d5
cherrytrees-kpu <54606285+cherrytrees-kpu@users.noreply.github.com>
2020-10-27 17:54:55 -0700

    Update getMissenseTranscript.py
    Removed canonical

--- getMissenseTranscript.py
+++ getMissenseTranscript.py
@@ -1,4 +1,3 @@
-from Bio.Seq import Seq
 import sqlite3
 import requests
 
@@ -35,8 +34,6 @@ if __name__ == '__main__':
         MostSevereConsequence IN ('missense_variant', 'inframe_insertion', 'splice_region_variant', 'regulatory_region_variant', 'stop_gained', 'frameshift_variant', 'splice_acceptor_variant', 'splice_donor_variant', 'stop_retained_variant', 'start_lost', 'stop_lost', 'inframe_deletion')
         AND (gnomADE < 0.001 OR gnomADG < 0.001 OR (gnomADG IS NULL AND gnomADE IS NULL))
         )
-        AND
-        Canonical = 1
         AND
         Consequence IN ('missense_variant')
         '''
